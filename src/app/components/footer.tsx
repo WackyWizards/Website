4511a8c42c5f684d6bd42c9a0a22c5d9d099ec87
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-06-21 22:29:35 +0300

    Add privacy policy link to footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,13 +1,14 @@
 import { OrgName } from "@/constants";
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <footer className="bg-gray-900 text-white p-4 mt-10 fixed bottom-0 w-full z-10">
             <div className="flex items-center w-full">
                 <p className="text-sm flex-grow text-left">© 2025 {OrgName}. All rights reserved.</p>
-                {/*<Link href="/privacypolicy" className="text-sm whitespace-nowrap">
+                <Link href="/privacy" className="text-sm whitespace-nowrap">
                     Privacy Policy
-                </Link>*/}
+                </Link>
             </div>
         </footer>
     );
