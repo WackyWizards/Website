76102e47b8c0050f931b892efb5cf777adbe52be
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-31 21:07:55 +0300

    Remove layout metadata
    It was overriding other pages, which we don't want.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
-import { OrgName } from '@/constants';
 import { Suspense } from 'react';
-import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Analytics from '@/app/analytics';
 import CookieConsent from '@/app/components/cookieconsent';
-import './globals.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -17,29 +15,6 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
-
-export const metadata: Metadata = {
-  title: OrgName,
-  description: `${OrgName}'s Website`,
-  icons: {
-    icon: '/logo.ico',
-  },
-  openGraph: {
-    title: OrgName,
-    description: `${OrgName}'s Website`,
-    url: 'https://www.kuo-team.com',
-    siteName: OrgName,
-    images: [
-      {
-        url: '/logo.png',
-        width: 128,
-        height: 128,
-        alt: `${OrgName} Logo`,
-      },
-    ],
-    type: 'website',
-  },
-};
 
 export default function RootLayout({
   children,
