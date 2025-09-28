33f21ff3757cd59b37612ed5125787bcf3ecde69
kEllieDev <kEllieDev@users.noreply.github.com>
2025-09-28 16:46:43 +0300

    Add main page metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-
+import { Metadata } from 'next';
 import Games from '@/app/components/games';
 import Team from '@/app/components/team';
-
 import { organization } from '@/constants';
+
+export const metadata: Metadata = {
+  title: organization.name,
+  description: organization.description,
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: [
+      { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' }
+    ]
+  },
+  openGraph: {
+    title: organization.name,
+    description: organization.description,
+    url: organization.website,
+    siteName: organization.name,
+    images: [],
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: organization.name,
+    description: organization.description
+  }
+};
 
 type BackgroundType = { type: 'image' | 'video'; src: string };
 const adjectives: string[] = [
