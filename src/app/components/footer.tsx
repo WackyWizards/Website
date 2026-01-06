89cfefcac1d64b0f5c349d1e60ac4e888a90de02
kellie <kEllieDev@users.noreply.github.com>
2026-01-07 00:10:26 +0200

    Update copyright year

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
         <div className="border-t border-gray-800 mt-12 pt-8">
           <div className="flex flex-col md:flex-row items-center md:justify-between text-sm text-gray-400">
             <span className="w-full md:w-auto md:flex-grow">
-              © 2025 {organization.name}. All rights reserved.
+              © 2026 {organization.name}. All rights reserved.
             </span>
 
             <div className="flex space-x-6 mt-4 md:mt-0">
